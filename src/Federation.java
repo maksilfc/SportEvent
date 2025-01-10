7d9292aea7ff8b7153681f8c8070c74e9cc15b2c
smgB6D9 <maksi.17kostov@abv.bg>
2025-01-10 16:32:28 +0200

    federation

--- src/Federation.java
+++ src/Federation.java
@@ -1,5 +1,3 @@
-import java.util.List;
-
 public class Federation {
     private int id;
     private String name;
@@ -7,9 +5,6 @@ public class Federation {
     private int boardMembersNumber;
     private int yearOfCreation;
     private double funding;
-    private List<Contest> contestsOrganized;
-
-    public Federation() {}
 
     public Federation(int id, String name, String presidentName, int boardMembersNumber, int yearOfCreation, double funding) {
         this.id = id;
@@ -20,18 +15,63 @@ public class Federation {
         this.funding = funding;
     }
 
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-    public String getPresidentName() { return presidentName; }
-    public void setPresidentName(String presidentName) { this.presidentName = presidentName; }
-    public int getBoardMembersNumber() { return boardMembersNumber; }
-    public void setBoardMembersNumber(int boardMembersNumber) { this.boardMembersNumber = boardMembersNumber; }
-    public int getYearOfCreation() { return yearOfCreation; }
-    public void setYearOfCreation(int yearOfCreation) { this.yearOfCreation = yearOfCreation; }
-    public double getFunding() { return funding; }
-    public void setFunding(double funding) { this.funding = funding; }
-    public List<Contest> getContestsOrganized() { return contestsOrganized; }
-    public void setContestsOrganized(List<Contest> contestsOrganized) { this.contestsOrganized = contestsOrganized; }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPresidentName() {
+        return presidentName;
+    }
+
+    public void setPresidentName(String presidentName) {
+        this.presidentName = presidentName;
+    }
+
+    public int getBoardMembersNumber() {
+        return boardMembersNumber;
+    }
+
+    public void setBoardMembersNumber(int boardMembersNumber) {
+        this.boardMembersNumber = boardMembersNumber;
+    }
+
+    public int getYearOfCreation() {
+        return yearOfCreation;
+    }
+
+    public void setYearOfCreation(int yearOfCreation) {
+        this.yearOfCreation = yearOfCreation;
+    }
+
+    public double getFunding() {
+        return funding;
+    }
+
+    public void setFunding(double funding) {
+        this.funding = funding;
+    }
+
+    @Override
+    public String toString() {
+        return "Federation{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", presidentName='" + presidentName + '\'' +
+                ", boardMembersNumber=" + boardMembersNumber +
+                ", yearOfCreation=" + yearOfCreation +
+                ", funding=" + funding +
+                '}';
+    }
 }
